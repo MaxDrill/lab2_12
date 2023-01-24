fadc68f7d58e852ce062071dca2bd47023383b01
MaxDrill <konyjeff@yandex.ru>
2023-01-24 14:20:30 +0300

    add proj

---
+++ proj/lab15_idz.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def sort_list(func):
+    """
+    Сортировка списка
+    """
+    def wrapper(*args):
+        arg = args[0]
+        sort_line = sorted(func(arg))
+        return sort_line
+    return wrapper
+
+
+@sort_list
+def get_list(line):
+    """
+    Создание списка из строки цифр разделенных пробелами
+    """
+    new_line = [int(i) for i in line.split()]
+    return new_line
+
+
+if __name__ == '__main__':
+    s = input("Enter line: ")
+    print(get_list(s))
